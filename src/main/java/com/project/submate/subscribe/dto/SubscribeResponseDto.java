66663ca9918e0f2abc8f4410565394b5591b38f4
jisooou <jisoou4@gmail.com>
2025-05-17 15:59:32 +0900

    feat: subscribeNo로 구독 조회

--- src/main/java/com/project/submate/subscribe/dto/SubscribeResponseDto.java
+++ src/main/java/com/project/submate/subscribe/dto/SubscribeResponseDto.java
@@ -1,6 +1,7 @@
 package com.project.submate.subscribe.dto;
 
 import com.project.submate.subscribe.entity.Subscribe;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -8,16 +9,25 @@ import java.time.LocalDate;
 
 @Getter
 @AllArgsConstructor
+@Schema(description = "등록한 구독 서비스 조회")
 public class SubscribeResponseDto {
+    @Schema(description = "구독 번호", example = "1")
     private Integer subscribeNo;
+    @Schema(description = "구독 이름", example = "넷플릭스")
     private String subscribeName;
+    @Schema(description = "구독 로고", example = "S3에 배포한 netflix.png")
     private String image;
+    @Schema(description = "구독 요금", example = "12000")
     private Integer price;
+    @Schema(description = "구독 가격단위", example = "원")
     private String priceUnit;
+    @Schema(description = "구독 주기", example = "1")
     private Integer period;
+    @Schema(description = "구독 주기 단위", example = "달")
     private String periodUnit;
+    @Schema(description = "구독 시작 날짜", example = "2025-05-17")
     private LocalDate startDate;
-    private String isCollect;
+//    private String isCollect;
 
     public static SubscribeResponseDto from(Subscribe s) {
         return new SubscribeResponseDto(
@@ -28,8 +38,8 @@ public class SubscribeResponseDto {
                 s.getPriceUnit(),
                 s.getPeriod(),
                 s.getPeriodUnit(),
-                s.getStartDate(),
-                s.getIsCollect()
+                s.getStartDate()
+//                s.getIsCollect()
         );
     }
 }
--- src/main/java/com/project/submate/subscribe/dto/SubscribeSearchResponseDto.java
+++ src/main/java/com/project/submate/subscribe/dto/SubscribeSearchResponseDto.java
@@ -7,24 +7,24 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Getter
 @Schema(description = "구독서비스 이름으로 검색")
 public class SubscribeSearchResponseDto {
-    @Schema(description = "구독서비스 번호", example = "1")
-    private Integer submateNo;
-
-    @Schema(description = "구독서비스 이름", example = "넷플릭스")
-    private String name;
-
-    @Schema(description = "구독서비스 이미지 URL", example = "https://netflix.com/logo.png")
-    private String image;
-
-    public Integer getSubmateNo() {
-        return submateNo;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getImage() {
-        return image;
-    }
+//    @Schema(description = "구독서비스 번호", example = "1")
+//    private Integer submateNo;
+//
+//    @Schema(description = "구독서비스 이름", example = "넷플릭스")
+//    private String name;
+//
+//    @Schema(description = "구독서비스 이미지 URL", example = "https://netflix.com/logo.png")
+//    private String image;
+//
+//    public Integer getSubmateNo() {
+//        return submateNo;
+//    }
+//
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public String getImage() {
+//        return image;
+//    }
 }
