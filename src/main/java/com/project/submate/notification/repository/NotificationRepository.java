484e2b006d97a3f1a6c7c947b011bdb7c813e1ff
jisooou <jisoou4@gmail.com>
2025-06-09 00:32:53 +0900

    feat: 스케줄러를 이용한 notification 기능 구현 다시 추가

--- src/main/java/com/project/submate/notification/repository/NotificationRepository.java
+++ src/main/java/com/project/submate/notification/repository/NotificationRepository.java
@@ -1,10 +1,10 @@
-//package com.project.submate.notification.repository;
-//
-//import com.project.submate.notification.entity.Notification;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//import java.util.List;
-//
-//public interface NotificationRepository extends JpaRepository<Notification, Integer> {
-//    List<Notification> findAllByUserIdAndIsReadFalse(Integer userId);
-//}
+package com.project.submate.notification.repository;
+
+import com.project.submate.notification.entity.Notification;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface NotificationRepository extends JpaRepository<Notification, Integer> {
+    List<Notification> findAllByUserIdAndIsReadFalse(Integer userId);
+}
