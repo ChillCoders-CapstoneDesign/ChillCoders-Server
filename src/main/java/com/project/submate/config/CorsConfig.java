d3d9bb2620499b067c33423ffebcdf4c5cd71cc8
jisooou <jisoou4@gmail.com>
2025-06-12 03:28:07 +0900

    feat: CorsConfig 작성

---
+++ src/main/java/com/project/submate/config/CorsConfig.java
@@ -0,0 +1,25 @@
+package com.project.submate.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**") // 전체 API 허용
+                        .allowedOrigins("http://localhost:3000") // 현재 프론트 도메인
+                        .allowedMethods("*")
+                        .allowedHeaders("*")
+                        .allowCredentials(true); // 인증정보 포함 허용
+            }
+        };
+    }
+}
